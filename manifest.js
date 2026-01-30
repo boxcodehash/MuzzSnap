22ca8268c95c3c203e8318dc726661927ec65276
boxcodehash <boxtvpr1@gmail.com>
2026-01-30 19:38:19 -0400

    Update and rename muzz-core.js to manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "MuzzSnap Protocol",
+  "short_name": "MuzzSnap",
+  "start_url": "login.html",
+  "display": "standalone",
+  "background_color": "#050505",
+  "theme_color": "#ff0000",
+  "icons": [
+    {
+      "src": "https://img1.wsimg.com/isteam/ip/ac9578cb-a6ce-417e-8ad5-b2d6701462eb/1000030902.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "https://img1.wsimg.com/isteam/ip/ac9578cb-a6ce-417e-8ad5-b2d6701462eb/1000030902.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
