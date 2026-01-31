2aaca3812b08a41ab6837536f623805de5191518
boxcodehash <boxtvpr1@gmail.com>
2026-01-30 20:10:22 -0400

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,19 +1,19 @@
 {
   "name": "MuzzSnap Protocol",
   "short_name": "MuzzSnap",
-  "start_url": "index.html",
+  "start_url": "./index.html",
   "display": "standalone",
   "background_color": "#050505",
   "theme_color": "#ff0000",
+  "scope": "./",
   "icons": [
     {
-      "src": "icon-192.png",
+      "src": "https://img1.wsimg.com/isteam/ip/ac9578cb-a6ce-417e-8ad5-b2d6701462eb/1000030902.png",
       "sizes": "192x192",
-      "type": "image/png",
-      "purpose": "any maskable"
+      "type": "image/png"
     },
     {
-      "src": "icon-512.png,
+      "src": "https://img1.wsimg.com/isteam/ip/ac9578cb-a6ce-417e-8ad5-b2d6701462eb/1000030902.png",
       "sizes": "512x512",
       "type": "image/png"
     }
