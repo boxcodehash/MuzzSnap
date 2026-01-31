7c9fd80dfa99af1b9688eafcbae033fe9184703c
boxcodehash <boxtvpr1@gmail.com>
2026-01-30 20:19:04 -0400

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,21 +1,24 @@
 {
   "name": "MuzzSnap Protocol",
   "short_name": "MuzzSnap",
-  "start_url": "./index.html",
+  "description": "Terminal Muzzle Protocol",
+  "start_url": "/MuzzSnap/index.html",
+  "scope": "/MuzzSnap/",
   "display": "standalone",
   "background_color": "#050505",
   "theme_color": "#ff0000",
-  "scope": "./",
   "icons": [
     {
       "src": "https://img1.wsimg.com/isteam/ip/ac9578cb-a6ce-417e-8ad5-b2d6701462eb/1000030902.png",
       "sizes": "192x192",
-      "type": "image/png"
+      "type": "image/png",
+      "purpose": "any"
     },
     {
       "src": "https://img1.wsimg.com/isteam/ip/ac9578cb-a6ce-417e-8ad5-b2d6701462eb/1000030902.png",
       "sizes": "512x512",
-      "type": "image/png"
+      "type": "image/png",
+      "purpose": "any"
     }
   ]
 }
